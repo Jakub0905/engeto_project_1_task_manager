888365e35500ff48e6e6702d37756a1362666e48
Jakub0905 <jakubzbavitel2005@seznam.cz>
2026-03-15 12:10:05 +0100

    Add files via upload

---
+++ task_manager.py
@@ -0,0 +1,56 @@
+ukoly = []
+
+def hlavni_menu():
+    while True:
+        print("Správce úkolů - Hlavní menu")
+        print("1. Přidat nový úkol")
+        print("2. Zobrazit všechny úkoly")
+        print("3. Odstranit úkol")
+        print("4. Konec programu")
+        volba = input("Vyberte možnost (1-4): ")
+
+        if volba == "1":
+            pridat_ukol()
+        elif volba == "2":
+            zobrazit_ukoly()
+        elif volba == "3":
+            odstranit_ukol()
+        elif volba == "4":
+            print("Konec programu.")
+            break
+        else:
+            print("Neplatná volba. Zkuste to znovu.")
+
+def pridat_ukol():
+    while True:
+        nazev = input("Zadejte název úkolu: ")
+        if nazev == "":
+            print("Název úkolu nemůže být prázdný.")
+        else:
+            break
+    ukoly.append({"nazev": nazev})
+    print(f"Úkol '{nazev}' byl přidán.")
+
+def zobrazit_ukoly():
+    print("Seznam úkolů:")
+    if not ukoly:
+        print("Žádné úkoly.")
+    else:
+        for i, ukol in enumerate(ukoly, 1):
+            print(f"{i}. {ukol['nazev']}")
+
+def odstranit_ukol():
+    zobrazit_ukoly()
+    if not ukoly:
+        return
+    while True:
+        cislo = input("Zadejte číslo úkolu, který chcete odstranit: ")
+        if cislo.isdigit() and 1 <= int(cislo) <= len(ukoly):
+            cislo = int(cislo) - 1
+            print(f"Úkol '{ukoly[cislo]['nazev']}' byl odstraněn.")
+            ukoly.pop(cislo)
+            break
+        else:
+            print("Neplatné číslo úkolu.")
+
+hlavni_menu()
